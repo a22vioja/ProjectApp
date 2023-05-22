8e1ab3ea8d629ff3bb0a21e9af3874832260e8f1
a22vioja <a22vioja@student.his.se>
2023-05-22 09:55:33 +0200

    implemented button for about page, added back to Main function

--- app/src/main/java/com/example/projectapp/MainActivity.java
+++ app/src/main/java/com/example/projectapp/MainActivity.java
@@ -15,12 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonExplore= findViewById(R.id.buttonExplore);
+
         buttonExplore.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 // makes the button in MainActivity page open About page screen
                 Intent intent = new Intent(MainActivity.this, BonsaiPage.class);
                 startActivity(intent);
 
+            }
+        });
+        Button buttonAbout= findViewById(R.id.buttonAbout);
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // makes the button in MainActivity page open About page screen
+                Intent intent = new Intent(MainActivity.this, About.class);
+                startActivity(intent);
             }
         });
     }
