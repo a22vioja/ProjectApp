2562fe745d69edc5b01fac5f5514d1b76340c42e
a22vioja <a22vioja@student.his.se>
2023-05-22 09:15:10 +0200

    added Bonsai item page and MyAdapter to get the data from Json

--- app/src/main/java/com/example/projectapp/Bonsai.java
+++ app/src/main/java/com/example/projectapp/Bonsai.java
@@ -1,4 +1,80 @@
 package com.example.projectapp;
 
+import com.google.gson.annotations.SerializedName;
+import android.content.Context;
+import android.view.View;
+
+@SuppressWarnings("WeakerAccess")
 public class Bonsai {
+    @SerializedName("Name")
+    public String name;
+    @SerializedName("ID")
+    public String id;
+    @SerializedName("Size")
+    public String height;
+
+    @SerializedName("Cost")
+    public int price;
+
+    @SerializedName("Category")
+    public String category;
+
+    @SerializedName("Login")
+    public String login;
+
+    public Bonsai(String name, String id, String height, int price, String category, String login) {
+        this.name = name;
+        this.id = id;
+        this.height = height;
+        this.price = price;
+        this.category = category;
+        this.login = login;
+    }
+
+  /*  public String info() {
+        String tmp = new String();
+        tmp += "Name:" + name + " ID:" + id + "Cost: " + price;
+        return tmp;
+    }*/
+
+    /*
+        public Bonsai(String name, String ID, int size, int price, Context context) {
+            super(context);
+            this.name = name;
+            this.id = id;
+            this.height = height;
+            this.price = price;
+        }
+
+        public Bonsai(String s, String i, Context context) {
+            super(context);
+            name = "No name";
+            id = "No id";
+            height = "No height";
+            price = 0;
+        }
+    */
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public int getPrice() {
+        return price;
+    }
+
+
+
 }
+
--- app/src/main/java/com/example/projectapp/BonsaiPage.java
+++ app/src/main/java/com/example/projectapp/BonsaiPage.java
@@ -1,26 +1,59 @@
 package com.example.projectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import com.example.projectapp.Bonsai;
 
 public class BonsaiPage extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a22vioja";
     ArrayList<Bonsai> listOfBonsai;
-    private RecyclerView.ViewHolder holder;
+   // private RecyclerView.ViewHolder holder;
+    MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bonsai_page);
+
+        listOfBonsai = new ArrayList<Bonsai>();
+
+        adapter=new MyAdapter(this, listOfBonsai, new MyAdapter.OnClickListener() {
+            @Override
+            public void onClick(Bonsai item) {
+                Toast.makeText(BonsaiPage.this, item.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
+
+        new JsonTask(this).execute(JSON_URL);
+
     }
 
     @Override
     public void onPostExecute(String json) {
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Bonsai>>() {}.getType();
+
+        adapter.addData(listOfBonsai);
+
+        ArrayList<Bonsai> listOfBonsai = gson.fromJson(json, type);
+
+        adapter.notifyDataSetChanged();
 
     }
 }
