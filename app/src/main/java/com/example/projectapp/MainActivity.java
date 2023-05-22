84b4b42cbd33da6c500680879de325b0a30d7403
a22vioja <a22vioja@student.his.se>
2023-05-22 08:40:49 +0200

    Created Main page with the button to the recycler view activity (BonsaiPage)

--- app/src/main/java/com/example/projectapp/MainActivity.java
+++ app/src/main/java/com/example/projectapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.projectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonExplore= findViewById(R.id.buttonExplore);
+        buttonExplore.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // makes the button in MainActivity page open About page screen
+                Intent intent = new Intent(MainActivity.this, BonsaiPage.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
