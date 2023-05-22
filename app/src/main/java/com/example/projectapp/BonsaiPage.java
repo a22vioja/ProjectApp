5b60663dedb6b60794fb54aa6edf36cc9242f731
a22vioja <a22vioja@student.his.se>
2023-05-22 08:59:32 +0200

    added Bonsai item class and Json task

--- app/src/main/java/com/example/projectapp/BonsaiPage.java
+++ app/src/main/java/com/example/projectapp/BonsaiPage.java
@@ -1,14 +1,26 @@
 package com.example.projectapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class BonsaiPage extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class BonsaiPage extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a22vioja";
+    ArrayList<Bonsai> listOfBonsai;
+    private RecyclerView.ViewHolder holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bonsai_page);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+
     }
 }
